fa989ae85f11eb5a5218065e0016fcdf8c79760e
coreyhines <chines@arista.com>
2021-01-15 21:10:39 +0000

    put code under a proper main() function

--- config-tool.py
+++ config-tool.py
@@ -51,75 +51,80 @@ def search_comments(line):
     return list(re_match)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-c", "--count", type=str, default="3", choices=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "all"],
-    help="specify min count", required=False)
-parser.add_argument(
-    "-m", "--mask", type=str, default="",
-    help="specify a string to ignore, e.g. 'description'\n to ingnore and replace descriptions", required=False)
-parser.add_argument(
-    "-d", "--directory", type=str, default="",
-    help="directory that contains EOS configuration files", required=False)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+       "-c", "--count", type=str, default="3", choices=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "all"],
+       help="specify min count", required=False)
+    parser.add_argument(
+        "-m", "--mask", type=str, default="",
+        help="specify a string to ignore, e.g. 'description'\n to ingnore and replace descriptions", required=False)
+    parser.add_argument(
+        "-d", "--directory", type=str, default="",
+     help="directory that contains EOS configuration files", required=False)
+    args = parser.parse_args()
 
-stanzas = []
-comments = []
+    stanzas = []
+    comments = []
 
-home = expanduser("~")
-if args.directory:
-    mydir = args.directory
-else:
-    mydir = home + "/vs-code/config-tool/configs/"
+    home = expanduser("~")
+    if args.directory:
+        mydir = args.directory
+    else:
+        mydir = home + "/vs-code/config-tool/configs/"
 
-num_files = str(
-    len([name for name in os.listdir(mydir) if os.path.isfile(mydir+'/'+name)]))
+    num_files = str(
+        len([name for name in os.listdir(mydir) if os.path.isfile(mydir+'/'+name)]))
 
-if args.count == "all":
-    mincount = num_files
-else:
-    mincount = args.count
+    if args.count == "all":
+        mincount = num_files
+    else:
+        mincount = args.count
 
-# Series of regex statements to remove extraneous unusable config patterns
-regex_sub = re.compile(r'^\s*!!.*|^>.*|^end.*|', re.M)
-regex_sub2 = re.compile(r'^\s.*Command:.*', re.M)
-regex_sub3 = re.compile(r'(^\s+)!(.*)', re.M)
-regex_sub4 = re.compile(r'^!.*boot\ssystem.*', re.M)
-regex_sub5 = re.compile(r'(^\s+)#(.*)', re.M)
+    # Series of regex statements to remove extraneous unusable config patterns
+    regex_sub = re.compile(r'^\s*!!.*|^>.*|^end.*|', re.M)
+    regex_sub2 = re.compile(r'^\s.*Command:.*', re.M)
+    regex_sub3 = re.compile(r'(^\s+)!(.*)', re.M)
+    regex_sub4 = re.compile(r'^!.*boot\ssystem.*', re.M)
+    regex_sub5 = re.compile(r'(^\s+)#(.*)', re.M)
 
-for path in pathlib.Path(mydir).iterdir():
-    if path.is_file():
-        current_file = open(path, "r")
-        content = current_file.read()
-        comments += search_comments(content)
-        subcontent = re.sub(regex_sub, "", content)
-        subcontent = re.sub(regex_sub2, "", subcontent)
-        # change any '!' with preceding whitespace to a '#'
-        # We will change those back at the end
-        subcontent = re.sub(regex_sub3, r"\1#\2", subcontent)
-        subcontent = re.sub(regex_sub4, "", subcontent)
-        if args.mask:
-            ignore_mask = args.mask
-            regex_sub6 = re.compile(
-                r'(.*{}).*'.format(ignore_mask), re.M | re.IGNORECASE)
-            subcontent = re.sub(regex_sub6, r"\1", subcontent)
+    for path in pathlib.Path(mydir).iterdir():
+        if path.is_file():
+            current_file = open(path, "r")
+            content = current_file.read()
+            comments += search_comments(content)
+            subcontent = re.sub(regex_sub, "", content)
+            subcontent = re.sub(regex_sub2, "", subcontent)
+            # change any '!' with preceding whitespace to a '#'
+            # We will change those back at the end
+            subcontent = re.sub(regex_sub3, r"\1#\2", subcontent)
+            subcontent = re.sub(regex_sub4, "", subcontent)
+            if args.mask:
+                ignore_mask = args.mask
+                regex_sub6 = re.compile(
+                    r'(.*{}).*'.format(ignore_mask), re.M | re.IGNORECASE)
+                subcontent = re.sub(regex_sub6, r"\1", subcontent)
 
-        stanzas += subcontent.split('!')
-        current_file.close()
+            stanzas += subcontent.split('!')
+            current_file.close()
 
-# print statements for debugging/testing
-# print(len(stanzas))
+    # print statements for debugging/testing
+    # print(len(stanzas))
 
-# This loop will print only stanzas that were seen 'min_count' or more times
-for k, v in sorted(Counter(stanzas).items()):
-    if v >= int(mincount) and v <= int(num_files):
-        # substitute the '  !' back in for the '#' used to trick the split parser earlier
-        print(re.sub(regex_sub5, r"\1!\2", k))
-        print('\x1b[6;30;42m' + "↑ SEEN ->(" +
-              str(v) + "/" + num_files + ")<- TIMES ↑" + '\x1b[0m' + "\n")
+    # This loop will print only stanzas that were seen 'min_count' or more times
+    for k, v in sorted(Counter(stanzas).items()):
+        if v >= int(mincount) and v <= int(num_files):
+            # substitute the '  !' back in for the '#' used to trick the split parser earlier
+            print(re.sub(regex_sub5, r"\1!\2", k))
+            print('\x1b[6;30;42m' + "↑ SEEN ->(" +
+                  str(v) + "/" + num_files + ")<- TIMES ↑" + '\x1b[0m' + "\n")
 
-# Coments list for review
-# print(len(comments))
-comments = set(comments)
-print("\n\n##################### COMMENTS  '!!' found in corpus #######################")
-print("\n".join(comments))
+    # Coments list for review
+    # print(len(comments))
+    comments = set(comments)
+    print("\n\n##################### COMMENTS  '!!' found in corpus #######################")
+    print("\n".join(comments))
+
+
+if __name__ == '__main__':
+    main()
