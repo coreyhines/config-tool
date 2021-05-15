cde5def2843941200df8441c9e8abb8b25fda793
coreyhines <chines@arista.com>
2021-05-15 16:25:48 +0000

    refactored to add function for pulling running config

--- confgrabber.py
+++ confgrabber.py
@@ -5,6 +5,17 @@ import json
 import ssl
 import argparse
 import string
+import os
+
+
+def grabconfig(directory, user, passwd, host):
+    device = Server(
+        'https://{}:{}@{}/command-api'.format(user, passwd, host))
+    result = device.runCmds(
+        version=1, cmds=['enable', 'show running-config'], format='text')
+    with open(directory + '/' + host + '.txt', mode='wt', encoding='utf-8') as writer:
+        for lines in result[1]['output']:
+            writer.write(lines)
 
 
 def main():
@@ -13,16 +24,20 @@ def main():
          "-u", "--user", type=str, default="",
          help="specify a username", required=True)
      parser.add_argument(
+         "-p", "--passwd", type=str, default="",
+         help="for passing password interactively", required=True)
+     parser.add_argument(
          "-f", "--file", type=str, default="",
          help="specify a file with EOS Devices from which to pull the running-config", required=True)
      parser.add_argument(
-         "-p", "--passwd", type=str, default="",
-         help="for passing password interactively", required=True)
+         "-d", "--directory", type=str, default=".",
+         help="specify a directory to download configs to (note: no trailing '/'", required=False)
      args = parser.parse_args()
 
      file = args.file
      user = args.user
      passwd = args.passwd
+     directory = args.directory
 
      with open(file, 'r') as current_file:
          hostnames = current_file.readlines()
@@ -30,20 +45,22 @@ def main():
      _create_unverified_https_context = ssl._create_unverified_context
      ssl._create_default_https_context = _create_unverified_https_context
 
+# split the hostnames list array in N parts
+# create a thread for each array as a for loop of N subarrays
+# run grabconfig() in each thread on subarray of eos devices
+# execute threads
      for hostname in hostnames:
-        # _create_unverified_https_context = ssl._create_unverified_context
-        # ssl._create_default_https_context = _create_unverified_https_context
          host = hostname.strip()
-         try:
-            device = Server(
-                'https://{}:{}@{}/command-api'.format(user, passwd, host))
-            result = device.runCmds(
-                version=1, cmds=['enable', 'show running-config'], format='text')
-            with open('./' + host + '.txt', mode='wt', encoding='utf-8') as writer:
-               for lines in result[1]['output']:
-                   writer.write(lines)
-         except Exception as e:
-             print("something went wrong, check password\n\n" + str(e))
+         response = os.system(f"ping -c 2 {host} > /dev/null 2>&1")
+         if response == 0:
+             pingstatus = "Network Active"
+             try:
+                grabconfig(directory, user, passwd, host)
+             except Exception as e:
+                 print(f"something went wrong, check password\n\n{str(e)}\n")
+         else:
+           pingstatus = "Network Error"
+           print(f"{host} does not respond to ping, moving on..\n")
 
 
 if __name__ == '__main__':
