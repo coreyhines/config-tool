e3831cea52d2af44658896cd3213c68acd377b31
coreyhines <chines@arista.com>
2024-03-13 23:22:22 -0500

    multiprocessing cleanups

--- confgrabber.py
+++ confgrabber.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import multiprocessing
 import numpy as np
+import ping3
 
 
 def grab_config(hostnames, user, passwd, directory, sanitized):
@@ -21,8 +22,8 @@ def grab_config(hostnames, user, passwd, directory, sanitized):
         host = hostname.strip()
         _create_unverified_https_context = ssl._create_unverified_context
         ssl._create_default_https_context = _create_unverified_https_context
-        response = os.system(f"ping -c 2 {host} > /dev/null 2>&1")
-        if response == 0:
+        ping_result = ping3.ping(host, timeout=2, unit='ms')
+        if ping_result is not None:
             pingstatus = f"{host}: Network Active"
             print(pingstatus)
             try:
@@ -121,71 +122,24 @@ def main():
     # create a thread for each list
     # run grabconfig() in each thread on subarray of eos devices
     # execute threads
-    split = np.array_split(hostnames, 10)
-    arr1 = list(split[0])
-    arr2 = list(split[1])
-    arr3 = list(split[2])
-    arr4 = list(split[3])
-    arr5 = list(split[4])
-    arr6 = list(split[5])
-    arr7 = list(split[6])
-    arr8 = list(split[7])
-    arr9 = list(split[8])
-    arr10 = list(split[9])
-    t1 = multiprocessing.Process(
-        target=grab_config, args=(arr1, user, passwd, directory, sanitized)
-    )
-    t2 = multiprocessing.Process(
-        target=grab_config, args=(arr2, user, passwd, directory, sanitized)
-    )
-    t3 = multiprocessing.Process(
-        target=grab_config, args=(arr3, user, passwd, directory, sanitized)
-    )
-    t4 = multiprocessing.Process(
-        target=grab_config, args=(arr4, user, passwd, directory, sanitized)
-    )
-    t5 = multiprocessing.Process(
-        target=grab_config, args=(arr5, user, passwd, directory, sanitized)
-    )
-    t6 = multiprocessing.Process(
-        target=grab_config, args=(arr6, user, passwd, directory, sanitized)
-    )
-    t7 = multiprocessing.Process(
-        target=grab_config, args=(arr7, user, passwd, directory, sanitized)
-    )
-    t8 = multiprocessing.Process(
-        target=grab_config, args=(arr8, user, passwd, directory, sanitized)
-    )
-    t9 = multiprocessing.Process(
-        target=grab_config, args=(arr9, user, passwd, directory, sanitized)
-    )
-    t10 = multiprocessing.Process(
-        target=grab_config, args=(arr10, user, passwd, directory, sanitized)
-    )
-    # start threads in parallel
-    # start_time = time.time()
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
-    t9.start()
-    t10.start()
-    # end_time = time.time() - start_time
-    # join will wait for functions to finish
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    t5.join()
-    t6.join()
-    t7.join()
-    t8.join()
-    t9.join()
-    t10.join()
+
+    # Dynamically split hostnames based on CPU cores
+    num_cores = multiprocessing.cpu_count()
+    split = np.array_split(hostnames, num_cores)
+    processes = []
+
+    for arr in split:
+        p = multiprocessing.Process(
+            target=grab_config, args=(arr.tolist(), user, passwd, directory, args.sanitized)
+        )
+        processes.append(p)
+        p.start()
+
+    # wait for all processes to finish
+    for p in processes:
+        p.join()
+
+
     print(f"Done!")
     # print(
     #    f"Processing {len(hostnames)} EOS devices took {end_time} time using multiprocessing")
